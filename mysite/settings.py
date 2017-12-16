b6232ef163404916d015d3017d7f760bcadc82a4
ankit-2017 <ankitdubeymail@gmail.com>
2017-12-16 17:17:12 +0530

    Initial commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -139,13 +139,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-'''
 STATICFILES_DIRS = [
                 os.path.join(BASE_DIR, "static"),
             ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-'''
+
+DISABLE_COLLECTSTATIC = 1
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'mysite/media')
 
